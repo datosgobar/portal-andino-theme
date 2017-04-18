51ecfdc42876561073b35b2ffa844f2f7ac6f3eb
Julieta Colombo <jcolombo@devartis.com>
2017-04-18 11:38:55 -0300

    Agrego validaciones para form de de temas

---
+++ ckanext/gobar_theme/js/group/form_actions.js
@@ -0,0 +1,46 @@
+$(function () {
+    var submit_count = 0;
+    function formIsValid() {
+        $('.missing-field').remove();
+        var isValid = true;
+        var errorTemplate = '<div class="missing-field">Completá este dato</div>';
+
+        var title = $('#field-name');
+        if (!title.val().length > 0) {
+            isValid = false;
+            title.after(errorTemplate)
+        }
+
+        var url = $('#field-url');
+        var url_preview = $('.slug-preview-value')
+        if ($('.slug-preview').css('display') == 'none'){
+            if (!url.val().length > 0) {
+                isValid = false;
+                url.parent().after(errorTemplate)
+            }
+        } else {
+            if (url_preview.text() == '<nombre-del-grupo>') {
+                isValid = false;
+                $('.slug-preview').after(errorTemplate)
+            }
+        }
+
+        if (!isValid) {
+            window.scrollTo(0, 0);
+        }
+
+        return isValid;
+    };
+
+    $(document).on('click', '.btn-mini', function () {
+        if (submit_count > 0){
+            return formIsValid();
+        }
+    });
+
+    $('form#group-edit').submit(function () {
+        submit_count++;
+        return formIsValid();
+    });
+
+});
