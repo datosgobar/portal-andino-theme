f3e81721d75b7263b78a57d93dbaa8f735ae72bd
Julieta Colombo <jcolombo@devartis.com>
2017-05-16 10:56:50 -0300

    Elimino script

--- bin/build-combined-ckan-mo.sh
+++
@@ -1,4 +0,0 @@
-msgcat --use-first \
-    "/usr/lib/ckan/default/src/ckanext-gobar-theme/ckanext/gobar_theme/i18n/es/LC_MESSAGES/ckan.po" \
-    "/usr/lib/ckan/default/src/ckan/ckan/i18n/es/LC_MESSAGES/ckan.po" \
-| msgfmt - -o "/usr/lib/ckan/default/src/ckan/ckan/i18n/es/LC_MESSAGES/ckan.mo"
