de0a25467bf5c7ac0d2a4717f6a63a912d9c4eb9
Julieta Colombo <jcolombo@devartis.com>
2017-05-19 13:12:44 -0300

    Agrego test para eliminación de grupo

--- ckanext/gobar_theme/tests/test_plugin.py
+++ ckanext/gobar_theme/tests/test_plugin.py
@@ -1,5 +1,26 @@
-"""Tests for plugin.py."""
-import ckanext.gobar_theme.plugin as plugin
+import nose.tools
 
-def test_plugin():
-    pass
+import ckan.tests.helpers as helpers
+import ckan.tests.factories as factories
+import ckan.logic as logic
+import ckan.model as model
+
+
+assert_equals = nose.tools.assert_equals
+assert_raises = nose.tools.assert_raises
+eq = nose.tools.eq_
+ok = nose.tools.ok_
+raises = nose.tools.raises
+
+
+class TestDelete:
+    def setup(self):
+        helpers.reset_db()
+
+    def test_delete_group_purges_group(self):
+        group = factories.Group()
+
+        helpers.call_action('group_delete', id=group['name'])
+
+        assert_raises(logic.NotFound, helpers.call_action, 'group_show',
+                      context={}, id=group['name'])
