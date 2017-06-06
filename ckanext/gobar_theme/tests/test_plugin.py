75b203ef60181455489001dda1a671f89c2fa8a7
Julieta Colombo <jcolombo@devartis.com>
2017-06-06 09:34:05 -0300

    Remove test

--- ckanext/gobar_theme/tests/test_plugin.py
+++ ckanext/gobar_theme/tests/test_plugin.py
@@ -1,27 +1,4 @@
-import nose.tools
-
-import ckan.tests.helpers as helpers
-import ckan.tests.factories as factories
-import ckan.logic as logic
-import ckan.model as model
-
-
-assert_equals = nose.tools.assert_equals
-assert_raises = nose.tools.assert_raises
-eq = nose.tools.eq_
-ok = nose.tools.ok_
-raises = nose.tools.raises
-
-
-class TestDelete:
-    def setup(self):
-        helpers.reset_db()
-
-    def test_delete_group_purges_group(self):
-        sysadmin = factories.Sysadmin()
-        group = factories.Group(user=sysadmin)
-
-        helpers.call_action('group_delete', id=group['name'])
-
-        assert_raises(logic.NotFound, helpers.call_action, 'group_show',
-                      context={}, id=group['name'])
+"""Tests for plugin.py."""
+import ckanext.gobar_theme.plugin as plugin
+def test_plugin():
+    pass
