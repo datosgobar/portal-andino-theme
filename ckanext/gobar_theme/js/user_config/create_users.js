a0ec5f228ccdde13a0df2bdead76f6ab7a8657c3
Ignacio Heredia <ignacio.nh@gmail.com>
2017-06-15 15:00:43 -0300

    Validaciones y feedback para seccion de creacion de usuario

--- ckanext/gobar_theme/js/user_config/create_users.js
+++ ckanext/gobar_theme/js/user_config/create_users.js
@@ -16,4 +16,58 @@ $(function () {
         filter: true,
         selectAll: false
     });
+
+    $('input[name="username"]').on('change input keypress', function (e) {
+        var $input = $(e.currentTarget);
+        var remainingCaracters = 10 - $input.val().length;
+        var text = $input.val().length == 0 ? 'Máximo 10 caracteres.' : 'Restan ' + remainingCaracters.toString() + ' caracter' + (remainingCaracters == 1 ? '' : 'es')
+        $input.parent().siblings('.username-restriction').text(text)
+        clearFeedback($input)
+    })
+
+
+    var validateSection = function (createSection) {
+        var usernameInput = createSection.find('input[name="username"]')
+        var nameInput = createSection.find('input[name="fullname"]')
+        var emailInput = createSection.find('input[name="email"]')
+
+        clearFeedback(usernameInput)
+        clearFeedback(emailInput)
+        clearFeedback(nameInput)
+
+        if (usernameInput.val().length == 0) {
+            showNegativeFeedback(usernameInput, 'Completá este dato.')
+            return false
+        }
+        if (nameInput.val().length == 0) {
+            showNegativeFeedback(nameInput, 'Completá este dato.')
+            return false
+        }
+        if (emailInput.val().length == 0) {
+            showNegativeFeedback(emailInput, 'Completá este dato.')
+            return false
+        }
+        if (!email_re.test(emailInput.val())) {
+            showNegativeFeedback(emailInput, 'Usá este formato nombre@ejemplo.com.');
+            return false;
+        }
+        if (createSection.hasClass('user')) {
+            var selectedOrganizations = createSection.find('select.organization-select').multipleSelect('getSelects');
+            var divSelect = createSection.find('div.organization-select')
+            clearFeedback(divSelect)
+            if (selectedOrganizations.length == 0) {
+                showNegativeFeedback(divSelect, 'Completá este dato.')
+            }
+        }
+    };
+
+    $('.save-new-user').on('click', function (e) {
+        var createSection = $(e.currentTarget).parents('.create-section')
+        var isValid = validateSection(createSection);
+        if (isValid) {
+            var data = {
+
+            }
+        }
+    })
 });
--- ckanext/gobar_theme/js/user_config/my_account.js
+++ ckanext/gobar_theme/js/user_config/my_account.js
@@ -78,34 +78,12 @@ $(function () {
             showNegativeFeedback(secondInput, 'Usá por lo menos 4 caracteres.');
             return false
         } else if (attr == 'email') {
-            var email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             if (!email_re.test(secondInput.val())) {
                 showNegativeFeedback(secondInput, 'Usá este formato nombre@ejemplo.com.');
                 return false;
             }
         }
         return true
-    };
-
-    var showPositiveFeedback = function(input, msg) {
-        clearFeedback(input)
-        var errormsg = $('<p class="feedback validation-success"></p>').text(msg)
-        $(input).parent().append(errormsg)
-        var errorimg = $('<img src="/img/input-check.svg" class="feedback-img success">')
-        $(input).parent().append(errorimg)
-    };
-
-    var showNegativeFeedback = function (input, msg) {
-        clearFeedback(input)
-        var errormsg = $('<p class="feedback validation-error"></p>').text(msg)
-        $(input).parent().append(errormsg)
-        var errorimg = $('<img src="/img/input-error.svg" class="feedback-img">')
-        $(input).parent().append(errorimg)
-    };
-
-    var clearFeedback = function (input) {
-        $(input).parent().find('.feedback').remove()
-        $(input).parent().find('.feedback-img').remove()
     };
 
     $('#save-email, #save-password').click(function (e) {
@@ -115,6 +93,4 @@ $(function () {
             sendChanges(editSection)
         }
     });
-
-    $('')
 });
