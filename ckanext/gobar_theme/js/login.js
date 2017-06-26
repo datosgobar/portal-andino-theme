6eeb5b6afd466013d94586cb9cbdba5dd8ce0d4d
Ignacio Heredia <ignacio.nh@gmail.com>
2017-06-26 12:40:03 -0300

    Acciones para modal de "olvidé mi contraseña"

--- ckanext/gobar_theme/js/login.js
+++ ckanext/gobar_theme/js/login.js
@@ -2,4 +2,24 @@ $(function () {
     $('#password-reset-show-modal').click(function () {
         $('.modal').modal('show')
     });
+    $('#send-recovery').on('click', function () {
+        var userInput = $('#username');
+        clearFeedback(userInput);
+        var username = userInput.val().trim();
+        if (!username || username.length == 0) {
+            showNegativeFeedback(userInput, 'Completá este dato.')
+            return
+        }
+        var callback = function (response) {
+            if (!response.success) {
+                showNegativeFeedback(userInput, '¡Oh! No encontramos este usuario. Probá con otro.')
+            } else {
+                showPositiveFeedback(userInput, '¡Perfecto! Te enviamos un e-mail para que crear una nueva contraseña.')
+            }
+        };
+        var failCallback = function () {
+            var feedback = '¡Oh! No encontramos este usuario. Probá con otro.'
+        };
+        $.post('/olvide_mi_contraseña', {user: username}, callback).fail(failCallback);
+    })
 });
