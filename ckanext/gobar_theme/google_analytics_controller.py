f08de32281e2aa273a90d32ef8332673ebb85c50
Julieta Colombo <jcolombo@devartis.com>
2017-06-28 15:41:54 -0300

    Agrego linea vacía de prueba

--- ckanext/gobar_theme/google_analytics_controller.py
+++ ckanext/gobar_theme/google_analytics_controller.py
@@ -3,5 +3,6 @@ from ckan.lib.base import BaseController, c
 
 
 class GobArGAController(BaseController):
+    
     def resource_view_embed(self, resource_id):
         google_analytics._post_analytics(c.user, 'CKAN Resource Embed', 'Resource ', resource_id, resource_id)
