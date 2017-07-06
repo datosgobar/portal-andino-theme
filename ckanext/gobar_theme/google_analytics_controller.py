2e200929dee884d700b527255a047411a95e181a
Julieta Colombo <jcolombo@devartis.com>
2017-07-06 11:58:13 -0300

    Elimino un parámetro cuando se llama a analytics por un recurso embebido

--- ckanext/gobar_theme/google_analytics_controller.py
+++ ckanext/gobar_theme/google_analytics_controller.py
@@ -5,4 +5,4 @@ from ckan.lib.base import BaseController, c
 class GobArGAController(BaseController):
     
     def resource_view_embed(self, resource_id):
-        google_analytics._post_analytics(c.user, 'CKAN Resource Embed', 'Resource ', resource_id, resource_id)
+        google_analytics._post_analytics(c.user, 'CKAN Resource Embed', 'Resource ', resource_id)
