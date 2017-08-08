57c00ddbdf448fc0a2262f6382ae5393d2a7f0ce
Ignacio Heredia <ignacio.nh@gmail.com>
2017-08-08 17:39:18 -0300

    Cambio feedback de envio de mail de reseteo de contraseña

--- ckanext/gobar_theme/js/login.js
+++ ckanext/gobar_theme/js/login.js
@@ -10,17 +10,21 @@ $(function () {
             showNegativeFeedback(userInput, 'Completá este dato.')
             return
         }
+        var button = $('#send-recovery');
+        button.prop('disabled', true);
         var callback = function (response) {
             if (!response.success && response.error == 'not_found') {
+                button.prop('disabled', false);
                 showNegativeFeedback(userInput, '¡Oh! No encontramos este usuario. Probá con otro.')
             } else if (!response.success) {
+                button.prop('disabled', false);
                 if (response.error.indexOf('Connection refused') != -1) {
                     showNegativeFeedback(userInput, 'Hemos tenido un error al intentar enviarte un email. Contactate con tu administrador')
                 } else {
                     showNegativeFeedback(userInput, '') // TODO: mensaje de error inesperado
                 }
             } else {
-                showPositiveFeedback(userInput, '¡Perfecto! Te enviamos un e-mail para que crear una nueva contraseña.')
+                window.location.href = "/ingresar?email-feedback=true"
             }
         };
         var failCallback = function () {
