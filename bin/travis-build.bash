61e0d6f4e6c98b41aa6412bf8040c2fd5ae6f7fc
Christian Digiorno <cdigiorno@devartis.com>
2017-12-20 14:31:46 -0300

    Fix travis build > > Add Travis' PG service on version 9.1 so we don't need to install PG manually

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,32 +5,22 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install solr-jetty libcommons-fileupload-java
+sudo apt-get install solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
+export latest_ckan_release_branch='release-v2.5.3'
 echo "CKAN branch: $latest_ckan_release_branch"
 git checkout $latest_ckan_release_branch
-
 python setup.py develop
-#pip install -r requirements.txt --allow-all-external
-wget "https://raw.githubusercontent.com/datosgobar/portal-andino-theme/fix_build_travis/dev-requirements.txt" -O own_requirements.txt
-    # si esto llega a master, cambiar el link
-pip install -r own_requirements.txt --allow-all-external
-#pip install -r dev-requirements.txt --allow-all-external
+pip install -r requirements.txt --allow-all-external
+pip install -r dev-requirements.txt --allow-all-external
 cd -
 
 echo "Creating the PostgreSQL user and database..."
-#sudo apt-get install postgresql postgresql-contrib libpq-dev python-dev
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
-
-echo "SOLR config..."
-# Solr is multicore for tests on ckan master, but it's easier to run tests on
-# Travis single-core. See https://github.com/ckan/ckan/issues/2972
-sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
 
 echo "Initialising the database..."
 cd ckan
--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -3,12 +3,4 @@
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
-
-nosetests --ckan \
-          --nologcapture \
-          --with-pylons=subdir/test.ini \
-          --with-coverage \
-          --cover-package=ckanext.pages \
-          --cover-inclusive \
-          --cover-erase \
-          --cover-testswith-pylons=subdir/test.ini --with-coverage --cover-package=ckanext.gobar_theme --cover-inclusive --cover-erase --cover-tests
+nosetests --nologcapture --with-pylons=subdir/test.ini --with-coverage --cover-package=ckanext.gobar_theme --cover-inclusive --cover-erase --cover-tests
