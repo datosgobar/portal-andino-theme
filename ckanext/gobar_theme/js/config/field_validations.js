750d06ea2d7eb71cedb73a1354c4444701e8a38e
Christian Digiorno <cdigiorno@devartis.com>
2017-12-29 09:45:48 -0300

    Decomento validación de campos en javascript.

--- ckanext/gobar_theme/js/config/field_validations.js
+++
@@ -1,8 +0,0 @@
-$(function validateSiteFields(){
-    $(".site-config").on('click', function(e){
-        e.preventDefault();
-        if ($(".site-title").text().length < 9){
-            $(".site-title-div").append('<span">El título no puede tener menos de 9 caracteres.</span>');
-        }
-    });
-});
