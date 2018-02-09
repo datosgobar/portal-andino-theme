e8b3cd68b980a9623968d3a02f95820f3ff98c27
Poli García <poli@devartis.com>
2018-02-09 08:12:21 -0300

    Agrego endpoint y datos de municipios
    refs #136

--- ckanext/gobar_theme/spatial_controller.py
+++ ckanext/gobar_theme/spatial_controller.py
@@ -18,18 +18,26 @@ class GobArSpatialController(BaseController):
         with open(os.path.join(GobArSpatialController.SITE_ROOT, 'resources/provincias.json')) as file_handle:
             return file_handle.read()
 
-    def localidades(self):
+    def __local_administrative_unit(self, file_name, administrative_unit_name):
         response.content_type = 'application/json'
         
-        province_id = None
-        if 'provincia_id' in request.params.keys():
-            province_id = request.params.get('provincia_id')
+        province_ids = []
+        if 'provincia_id' in request.params.keys() and request.params.get('provincia_id'):
+            province_ids = request.params.get('provincia_id')
+            if not isinstance(province_ids, list):
+                province_ids = province_ids.split(',')
 
-        with open(os.path.join(GobArSpatialController.SITE_ROOT, 'resources/localidades.json')) as file_handle:
+        with open(os.path.join(GobArSpatialController.SITE_ROOT, 'resources/%s' % file_name)) as file_handle:
             districts = json.loads(file_handle.read())
 
             if province_id:
-                output_dict = [district for district in districts['localidades'] if district['provincia_id'] == province_id]
-                districts['localidades'] = output_dict
+                output_dict = [district for district in districts[administrative_unit_name] if district['provincia_id'] in province_ids]
+                districts[administrative_unit_name] = output_dict
 
             return json.dumps(districts)
+
+    def localidades(self):
+        return __local_administrative_unit('localidades.json', 'localidades')
+
+    def municipios(self):
+        return __local_administrative_unit('municipios.json', 'municipios')
