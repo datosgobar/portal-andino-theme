c2fc95fc641e658235577764f267a477013d5315
Christian Digiorno <cdigiorno@devartis.com>
2018-02-16 15:47:07 -0300

    Edito la tabla de visualización de metadatos de los recursos & agrego un script para su modificación en caso de que haya una scrollbar.

--- ckanext/gobar_theme/js/package/resource/resource_view.js
+++ ckanext/gobar_theme/js/package/resource/resource_view.js
@@ -1,9 +1,41 @@
 $(function () {
     $('a#embeber').on('click', function () {
-        var id = $('a#embeber').data('module-resource-id')
+        var id = $('a#embeber').data('module-resource-id');
          $.ajax({
             url: '/dataset/resource_view_embed/' + id,
             type: 'POST'
         });
     })
+});
+
+$(window).load(function(){
+    var body =  $('#metadata-table > tbody');
+    if (body.get(0).scrollHeight > body.height()){
+        alert("HOLA");
+        $('.col_title').each(function(index, element) {
+            element.style.setProperty( 'width', '13%', 'important' );
+        });
+        $('.type').each(function(index, element) {
+            element.style.setProperty( 'width', '7%', 'important' );
+        });
+        $('.description').each(function(index, element) {
+            element.style.setProperty( 'width', '34%', 'important' );
+        });
+        $('.units').each(function(index, element) {
+            element.style.setProperty( 'width', '16%', 'important' );
+        });
+        $('.id').each(function(index, element) {
+            element.style.setProperty( 'width', '17%', 'important' );
+        });
+        $('.specialType').each(function(index, element) {
+            element.style.setProperty( 'width', '7%', 'important' );
+        });
+        $('.specialTypeDetail').each(function(index, element) {
+            element.style.setProperty( 'width', '6%', 'important' );
+            element.style.setProperty( 'padding-right', '0', 'important' );
+        });
+    }
+    else{
+        alert("Nope");
+    }
 });
