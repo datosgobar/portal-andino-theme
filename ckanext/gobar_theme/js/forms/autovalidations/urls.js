161e8da7da5000fce363f439f18a873f95d8de7e
Poli García <poli@devartis.com>
2018-02-27 18:23:30 -0300

    Agrego validación para el campo URL de catalog_homepage
    Agrego una librería js que detecta si hay campos URL en el algún form y en ese caso los valida, en caso contrario deja un label con un error.
    
    refs #152

---
+++ ckanext/gobar_theme/js/forms/autovalidations/urls.js
@@ -0,0 +1,26 @@
+$(function() {
+
+    $('form:has(input.url-field[type=text])').on('submit', function(event) {
+        var URL_VALIDATION_REGEX = new RegExp(
+            /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-.]{1}[a-z0-9]+)*(\.[a-z0-9]{2,5}|(:[0-9]{1,5}))(\/.*)?$/
+        );
+
+        var HEADER_HEIGHT = $('#header').height();
+
+        $(event.target).find('input.url-field[type=text]').each(function(index) {
+            // Si la validación falla, abortar el submit y mostrar un mensaje de validación
+            var that = $(this);
+            if (that.val() && !that.val().match(URL_VALIDATION_REGEX)) {
+                // Agrego el mensaje de error
+                var errorMessageTemplate = '<label for="' + (that.attr('id') || '') +  '" class="form-error-message">La url ingresada no es válida</label>';
+                that.after(errorMessageTemplate);
+
+                $('html, body').animate({
+                    scrollTop: (that.first().offset().top - HEADER_HEIGHT)
+                }, 500);
+
+                event.preventDefault();  // Evitamos que el form haga el submit
+            }
+        })
+    });
+});
