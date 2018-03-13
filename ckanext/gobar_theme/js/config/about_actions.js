441369e78655d98084a5074dc0bba748c849383a
Christian Digiorno <cdigiorno@devartis.com>
2018-03-13 10:18:29 -0300

    Para cada sección, se escribe en el título 'Sección' + su índice correspondiente cada vez que se carga la página (si se eligieron las secciones avanzadas en el select), se borra una sección o se crea una nueva.

--- ckanext/gobar_theme/js/config/about_actions.js
+++ ckanext/gobar_theme/js/config/about_actions.js
@@ -2,7 +2,7 @@ $(window).on('load', function () {
     show_specific_content($('select').val());
 });
 
-$('select').on('change', function (e) {
+$('select').on('change', function () {
     show_specific_content($('select').val());
 });
 
@@ -18,16 +18,18 @@ function show_specific_content(selected_value) {
     else if (selected_value === 'Quiero secciones personalizadas (avanzado)') {
         $('div.basic-about').css('display', 'none');
         $('div.advanced-about').css('display', 'block');
+        writeDivIndexes();
     }
 }
 
 $('button#add-another-section').on('click', function (e) {
-   e.preventDefault();
+    e.preventDefault();
     var new_div = '<div class="section-div"><button type="button" class="close" aria-label="Close"><span aria-hidden="true">x</span></button><h2 class="section">Sección</h2><hr class="section"><h2>Título</h2><input class="about-section-title" name="about-section-title" type="text" title="Título" value=""><h2>Nombre del archivo</h2><input class="about-section-filename" name="about-section-filename" type="text" title="Nombre del archivo" value=""></div>';
     $('button#add-another-section').before(new_div);
+    writeDivIndexes();
 });
 
-$('form').on('submit', function (e) {
+$('form').on('submit', function () {
     var sections = [];
     $('div.section-div').each(function () {
         var all_inputs = $(this).find('input');
@@ -53,13 +55,23 @@ $('button.close').on('click', function () {
     var parent_div = $(this).parent();
     $('#delete-section-modal').modal(parent_div);
 
-    $(document).on('click', '#delete-section-btn', function(e) {
+    $(document).on('click', '#delete-section-btn', function () {
         var isTheOnlyOne = $('div.section-div').length === 1;
         if (isTheOnlyOne) {
-            parent_div.find('input').val('');  // Elimina los datos de la sección
+            parent_div.find('input').val('');  // Elimina los datos de la sección  todo: chequear que una seccion no se envie vacia
         } else {
             parent_div.remove();
         }
+        writeDivIndexes();
     });
-    
+
 });
+
+function writeDivIndexes() {
+    var cont = 1;
+    $('div.section-div').each(function () {
+        var section_h2 = $(this).find('h2.section');
+        section_h2.text("Sección " + cont);
+        cont += 1;
+    });
+}
