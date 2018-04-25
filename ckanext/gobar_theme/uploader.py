1dda710de8b54b7513be487b70e923c1ad0eaf34
Christian Digiorno <cdigiorno@devartis.com>
2018-04-25 10:29:27 -0300

    Cambio implementación de uploader de recursos propia de GobAr.

--- ckanext/gobar_theme/uploader.py
+++ ckanext/gobar_theme/uploader.py
@@ -20,31 +20,29 @@ class GobArThemeResourceUploader(uploader.ResourceUpload):
 
         # Hacer el init para el ícono `icon_upload`
 
-        self.filename = None
-
         resource_id = data_dict.get('id')
-        upload_field_storage = data_dict.pop('icon_upload', None)
-        self.clear = data_dict.pop('clear_icon_upload', None)
+        icon_upload_field_storage = data_dict.pop('icon_upload', None)
+        self.icon_clear = data_dict.pop('clear_icon_upload', None)
 
-        if isinstance(upload_field_storage, cgi.FieldStorage):
-            self.filename = '%s-%s' % (resource_id, upload_field_storage.filename)
-            self.upload_file = upload_field_storage.file
+        if isinstance(icon_upload_field_storage, cgi.FieldStorage):
+            self.icon_filename = '%s-%s' % (resource_id, icon_upload_field_storage.filename)
+            self.icon_upload_file = icon_upload_field_storage.file
 
-            data_dict['icon_url'] = config.get('ckan.site_url') + '/user_images/%s' % self.filename
+            data_dict['icon_url'] = config.get('ckan.site_url') + '/user_images/%s' % self.icon_filename
             data_dict['icon_url_type'] = 'upload'
-        elif self.clear:
+        elif self.icon_clear:
             data_dict['icon_url_type'] = ''
 
     def upload(self, id, max_size=10):
         super(GobArThemeResourceUploader, self).upload(id, max_size)
 
-        if not self.filename:
+        if not hasattr(self, 'icon_filename'):
             return
 
         # Hacer el upload para el ícono
-        output_path = os.path.join(GobArConfigController.IMG_DIR, self.filename)
+        output_path = os.path.join(GobArConfigController.IMG_DIR, self.icon_filename)
         output_file = open(output_path, 'wb')
-        upload_file = self.upload_file
+        upload_file = self.icon_upload_file
         upload_file.seek(0)
         while True:
             data = upload_file.read(2 ** 20)
