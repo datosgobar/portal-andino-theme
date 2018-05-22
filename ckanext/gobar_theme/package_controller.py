d884b26a631c57ec71c3a9be8271e0be223ac8e9
Christian Digiorno <cdigiorno@devartis.com>
2018-05-22 12:34:10 -0300

    Modificaciones en 'package_controller'.

--- ckanext/gobar_theme/package_controller.py
+++ ckanext/gobar_theme/package_controller.py
@@ -149,6 +149,7 @@ class GobArPackageController(PackageController):
                         and len(value) and not param.startswith('_'):
                     if not param.startswith('ext_'):
                         c.fields.append((param, value))
+                        # Modificación para andino: usamos una función para buscar dependencias entre organizaciones
                         if param != 'organization':
                             fq += ' %s:"%s"' % (param, value)
                         else:
@@ -220,6 +221,7 @@ class GobArPackageController(PackageController):
         c.search_facets_limits = {}
         for facet in c.search_facets.keys():
             try:
+                # Modificación para andino: chequeo si el facet es 'organization'
                 if facet != 'organization':
                     limit = int(request.params.get('_%s_limit' % facet, int(config.get('search.facets.default', 10))))
                 else:
@@ -370,67 +372,6 @@ class GobArPackageController(PackageController):
                                  errors, error_summary)
             data_dict['state'] = 'none'
             return self.new(data_dict, errors, error_summary)
-
-    def edit(self, id, data=None, errors=None, error_summary=None):
-        package_type = self._get_package_type(id)
-        context = {'model': model, 'session': model.Session,
-                   'user': c.user, 'auth_user_obj': c.userobj,
-                   'save': 'save' in request.params}
-
-        if context['save'] and not data:
-            return self._save_edit(id, context, package_type=package_type)
-        try:
-            c.pkg_dict = get_action('package_show')(dict(context,
-                                                         for_view=True),
-                                                    {'id': id})
-            context['for_edit'] = True
-            old_data = get_action('package_show')(context, {'id': id})
-            # old data is from the database and data is passed from the
-            # user if there is a validation error. Use users data if there.
-            if data:
-                old_data.update(data)
-            data = old_data
-        except (NotFound, NotAuthorized):
-            abort(404, _('Dataset not found'))
-        # are we doing a multiphase add?
-        if data.get('state', '').startswith('draft'):
-            c.form_action = h.url_for(controller='package', action='new')
-            c.form_style = 'new'
-            return self.new(data=data, errors=errors,
-                            error_summary=error_summary)
-
-        c.pkg = context.get("package")
-        c.resources_json = h.json.dumps(data.get('resources', []))
-
-        try:
-            check_access('package_update', context)
-        except NotAuthorized:
-            abort(403, _('User %r not authorized to edit %s') % (c.user, id))
-        # convert tags if not supplied in data
-        if data and not data.get('tag_string'):
-            data['tag_string'] = ', '.join(h.dict_list_reduce(
-                c.pkg_dict.get('tags', {}), 'name'))
-        errors = errors or {}
-        form_snippet = self._package_form(package_type=package_type)
-        form_vars = {'data': data, 'errors': errors,
-                     'error_summary': error_summary, 'action': 'edit',
-                     'dataset_type': package_type,
-                     }
-        c.errors_json = h.json.dumps(errors)
-
-        self._setup_template_variables(context, {'id': id},
-                                       package_type=package_type)
-
-        # we have already completed stage 1
-        form_vars['stage'] = ['active']
-        if data.get('state', '').startswith('draft'):
-            form_vars['stage'] = ['active', 'complete']
-
-        edit_template = self._edit_template(package_type)
-        return render(edit_template,
-                      extra_vars={'form_vars': form_vars,
-                                  'form_snippet': form_snippet,
-                                  'dataset_type': package_type})
 
     def _save_edit(self, name_or_id, context, package_type=None):
         from ckan.lib.search import SearchIndexError
