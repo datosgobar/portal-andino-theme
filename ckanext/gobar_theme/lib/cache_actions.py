e9050000c0f3960a2071f0baba3d539c34c8ddbd
Christian Digiorno <cdigiorno@devartis.com>
2018-07-04 13:05:34 -0300

    Utilizamos 'PURGE' como método default

--- ckanext/gobar_theme/lib/cache_actions.py
+++ ckanext/gobar_theme/lib/cache_actions.py
@@ -13,7 +13,8 @@ def clear_web_cache():
     cache_clean_hook_method = config.get('andino.cache_clean_hook_method')
     if cache_clean_hook is not None:
         try:
-            response_for_http_request = requests.request(cache_clean_hook_method, cache_clean_hook, timeout=2)
+            response_for_http_request = \
+                requests.request(cache_clean_hook_method or 'PURGE', cache_clean_hook, timeout=2)
             response_for_http_request.raise_for_status()
         except Exception as e:
             logger.info(u'Hubo un problema limpiando la caché del servicio web con el request a la url %s: %s ',
