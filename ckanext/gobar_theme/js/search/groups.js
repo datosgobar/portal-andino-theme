4566c82834b7f16ac2aa6d9691a3e0f312272f78
Poli García <poli@devartis.com>
2018-09-04 18:15:48 -0300

    Invoco a la función clamp junto al código que hace mostrar el contenido

--- ckanext/gobar_theme/js/search/groups.js
+++ ckanext/gobar_theme/js/search/groups.js
@@ -3,17 +3,17 @@ $(function () {
     var callback = function () {
         setTimeout(function () {
             $('.search-filter, #search-results').removeClass('invisible')
+            $('.dataset-title').each(function(index) {
+                $clamp(this, {clamp: 2})
+            });
+            $('.dataset-author').each(function(index) {
+                $clamp(this, {clamp: 1})
+            });
+            $('.dataset-notes').each(function(index) {
+                $clamp(this, {clamp: 2})
+            });
         }, 200)
     };
     $.when(calls).done(callback).fail(callback);
-
-    $('.dataset-title').each(function(index) {
-        $clamp(this, {clamp: 2, useNativeClamp: false})
-    });
-    $('.dataset-author').each(function(index) {
-        $clamp(this, {clamp: 1, useNativeClamp: false})
-    });
-    $('.dataset-notes').each(function(index) {
-        $clamp(this, {clamp: 2, useNativeClamp: false})
-    });
+    
 });
