3356693db3e1ec494cb571363ce1c2751122a57c
Poli García <poli@devartis.com>
2018-10-16 18:07:38 -0300

    Update travis-build.bash

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -16,9 +16,7 @@ sudo mv /opt/solr/solr/collection1 /opt/solr/solr/ckan
 echo "name=ckan" | sudo tee /opt/solr/solr/ckan/core.properties
 sudo wget https://raw.githubusercontent.com/ckan/ckan/ckan-2.7.4/ckan/config/solr/schema.xml -O /opt/solr/solr/ckan/conf/schema.xml
 sudo wget https://raw.githubusercontent.com/datosgobar/portal-base/master/solr/jetty-logging.xml -O /opt/solr/etc/jetty-logging.xml
-sudo ls -lsa /etc
-sudo ls -lsa /etc/default*
-curl https://raw.githubusercontent.com/datosgobar/portal-base/master/solr/jetty.defaults | sudo tee /etc/default/jetty > /dev/null
+echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo java -jar /opt/solr/start.jar --daemon
 cd -
 
