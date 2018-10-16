8b86212d36e254de75cde8141e24ba5d76832b67
Christian Digiorno <cdigiorno@devartis.com>
2018-10-16 12:23:35 -0300

    Modifico el manejo de los comandos necesarios para el testeo durantew el run de travis

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-psql -U ckan -h db -c "CREATE DATABASE datastore_test WITH OWNER ckan_default;"
-psql -U ckan -h db -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
+sudo -u postgres psql -c "CREATE DATABASE datastore_test WITH OWNER ckan_default;"
+sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
-nosetests --nocapture --nologcapture --ckan --reset-db --with-pylons=/home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/tests_config/test-core.ini
+nosetests --nocapture --nologcapture --ckan --reset-db --with-pylons=/home/travis/build/datosgobar/portal-andino-theme/ckan/test-core.ini
