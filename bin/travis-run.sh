95f0043c4444feda2d1b45c181a87e7b14873ca0
Christian Digiorno <cdigiorno@devartis.com>
2018-10-16 12:46:34 -0300

    Fix para install de factory

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -5,6 +5,7 @@ sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
+. /usr/lib/ckan/default/bin/activate
 cd /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/
 nosetests --nocapture --nologcapture --ckan --reset-db --with-pylons=/home/travis/build/datosgobar/portal-andino-theme/ckan/test-core.ini
 cd -
