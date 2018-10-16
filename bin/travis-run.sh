90cd1ffa1005584cc496071a33861de26c6586ff
Christian Digiorno <cdigiorno@devartis.com>
2018-10-16 15:19:03 -0300

    Cambio requerimiento para import de ckan

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -3,7 +3,6 @@
 sudo -u postgres psql -c "CREATE DATABASE datastore_test WITH OWNER ckan_default;"
 sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-pip install factory
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 cd /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/
