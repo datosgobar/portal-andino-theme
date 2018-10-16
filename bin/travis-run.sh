dbc8a4e8aa22ea1b52d98d01e4c443f5864204c7
Christian Digiorno <cdigiorno@devartis.com>
2018-10-16 15:41:55 -0300

    Uso nuestro test-core.ini en vez de el de ckan, realizando algunas ediciones en runtime para evitar problemas accediendo a postgres o solr

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -4,8 +4,9 @@ sudo -u postgres psql -c "CREATE DATABASE datastore_test WITH OWNER ckan_default
 sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
+sed -i 's/@db/@localhost/g' /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/tests_config/test-core.ini
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 cd /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/
-nosetests --nocapture --nologcapture --ckan --reset-db --with-pylons=/home/travis/build/datosgobar/portal-andino-theme/ckan/test-core.ini
+nosetests --nocapture --nologcapture --ckan --reset-db --with-pylons=/home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/tests_config/test-core.ini
 cd -
