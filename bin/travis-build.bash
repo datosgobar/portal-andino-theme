b7c5b97fcbab6013c1ff3c68daa4e6800aca94d0
Pablo García <poli@devartis.com>
2018-10-17 00:36:45 -0300

    Run solr differntly

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+is_solr_up(){
+    echo "Checking if solr is up on http://localhost:$SOLR_PORT/solr/admin/cores"
+    http_code=`echo $(curl -s -o /dev/null -w "%{http_code}" "http://localhost:$SOLR_PORT/solr/admin/cores")`
+    return `test $http_code = "200"`
+}
+
+wait_for_solr(){
+    while ! is_solr_up; do
+        sleep 3
+    done
+}
+
 echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
@@ -17,7 +29,10 @@ echo "name=ckan" | sudo tee /opt/solr/solr/ckan/core.properties
 sudo wget https://raw.githubusercontent.com/ckan/ckan/ckan-2.7.4/ckan/config/solr/schema.xml -O /opt/solr/solr/ckan/conf/schema.xml
 sudo wget https://raw.githubusercontent.com/datosgobar/portal-base/master/solr/jetty-logging.xml -O /opt/solr/etc/jetty-logging.xml
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-sudo java -jar /opt/solr/start.jar --daemon
+sudo java -Djetty.home=/opt/solr/ -Dsolr.solr.home=/opt/solr/solr/ -jar /opt/solr/start.jar &
+wait_for_solr
+echo "Started solr"
+
 cd -
 
 echo "Installing CKAN and its Python dependencies..."
