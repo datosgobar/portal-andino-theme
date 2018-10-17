7d4d1b55acbb1cd741a01e42aa16f77d283c3837
Pablo García <poli@devartis.com>
2018-10-17 00:40:08 -0300

    Fix solr port

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+SOLR_PORT=${SOLR_PORT:-8983}
+
 is_solr_up(){
     echo "Checking if solr is up on http://localhost:$SOLR_PORT/solr/admin/cores"
     http_code=`echo $(curl -s -o /dev/null -w "%{http_code}" "http://localhost:$SOLR_PORT/solr/admin/cores")`
@@ -28,7 +30,7 @@ sudo mv /opt/solr/solr/collection1 /opt/solr/solr/ckan
 echo "name=ckan" | sudo tee /opt/solr/solr/ckan/core.properties
 sudo wget https://raw.githubusercontent.com/ckan/ckan/ckan-2.7.4/ckan/config/solr/schema.xml -O /opt/solr/solr/ckan/conf/schema.xml
 sudo wget https://raw.githubusercontent.com/datosgobar/portal-base/master/solr/jetty-logging.xml -O /opt/solr/etc/jetty-logging.xml
-echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=${SOLR_PORT}\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo java -Djetty.home=/opt/solr/ -Dsolr.solr.home=/opt/solr/solr/ -jar /opt/solr/start.jar &
 wait_for_solr
 echo "Started solr"
