8da1bab0e7a51522695219f0c00221a9c14504a4
Christian Digiorno <cdigiorno@devartis.com>
2018-10-18 13:03:10 -0300

    Me fijo en el build si los 4 directorios se crean correctamente

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -5,6 +5,12 @@ sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 sed -i 's/@db/@localhost/g' /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/tests_config/test-core.ini
 
 cd /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/
+mkdir /var/
+if [ -d /var/ ] ; then     echo "Se creó bien el directorio /var/"; else     echo "No se pudo crear el directorio /var/"; fi
+mkdir /var/lib/
+if [ -d /var/lib/ ] ; then     echo "Se creó bien el directorio /var/lib/"; else     echo "No se pudo crear el directorio /var/lib/"; fi
+mkdir /var/lib/ckan/
+if [ -d /var/lib/ckan/ ] ; then     echo "Se creó bien el directorio /var/lib/ckan/"; else     echo "No se pudo crear el directorio /var/lib/ckan/"; fi
 mkdir /var/lib/ckan/theme_config/
 if [ -d /var/lib/ckan/theme_config/ ] ; then     echo "Se creó bien el directorio"; else     echo "No se pudo crear el directorio"; fi
 nosetests --nocapture --nologcapture --ckan --reset-db --with-pylons=/home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/tests_config/test-core.ini
