13a2e6abe4da0dd5ebd6c0345dc7da1bc594bd01
Christian Digiorno <cdigiorno@devartis.com>
2018-10-19 12:40:49 -0300

    Cambio de lugar chmod

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -9,8 +9,8 @@ sed -i 's/@db/@localhost/g' /home/travis/build/datosgobar/portal-andino-theme/ck
 cd /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/
 sudo mkdir /var/lib/ckan/
 sudo mkdir /var/lib/ckan/theme_config/
+sudo chmod -R 777 /var/lib/
 sudo echo "" > /var/lib/ckan/theme_config/test_settings.json
 sudo echo "" > /var/lib/ckan/theme_config/datajson_cache_backup.json
-sudo chmod -R 777 /var/lib/
 nosetests --nocapture --nologcapture --ckan --reset-db --with-pylons=/home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/tests_config/test-core.ini
 cd -
