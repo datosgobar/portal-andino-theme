09b380d7ba1bc7c4c312d068fe7f84d693c159f0
Christian Digiorno <cdigiorno@devartis.com>
2018-10-19 12:34:10 -0300

    Fix creando directorios en el build de travis

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -7,12 +7,10 @@ sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 sed -i 's/@db/@localhost/g' /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/tests_config/test-core.ini
 
 cd /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/
-sudo mkdir /var/
-sudo mkdir /var/lib/
 sudo mkdir /var/lib/ckan/
 sudo mkdir /var/lib/ckan/theme_config/
-echo "" > /var/lib/ckan/theme_config/test_settings.json
-echo "" > /var/lib/ckan/theme_config/datajson_cache_backup.json
+sudo echo "" > /var/lib/ckan/theme_config/test_settings.json
+sudo echo "" > /var/lib/ckan/theme_config/datajson_cache_backup.json
 sudo chmod -R 777 /var/lib/
 nosetests --nocapture --nologcapture --ckan --reset-db --with-pylons=/home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/tests_config/test-core.ini
 cd -
