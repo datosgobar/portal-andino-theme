2baa27df639a8c54de65bc004c44d361cfc7b6ed
Christian Digiorno <cdigiorno@devartis.com>
2018-10-23 15:50:58 -0300

    Elimino línea innecesaria

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -6,7 +6,6 @@ sudo -u postgres psql -c "CREATE DATABASE datastore_test WITH OWNER ckan_default
 sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 sed -i 's/@db/@localhost/g' /home/travis/build/datosgobar/portal-andino-theme/ckanext/gobar_theme/tests/tests_config/test-core.ini
 
-export TRAVIS_BUILD_DIR
 cd ${TRAVIS_BUILD_DIR}/ckanext/gobar_theme/tests/
 export CKAN_LIB=/var/lib/ckan
 sudo mkdir -p ${CKAN_LIB}/theme_config/
