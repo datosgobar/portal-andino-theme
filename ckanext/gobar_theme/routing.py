6670c6e1839124097ebebc76b97f92d7fd45c63a
Christian Digiorno <cdigiorno@devartis.com>
2018-12-27 10:40:12 -0300

    Fix routeo para secciones

--- ckanext/gobar_theme/routing.py
+++ ckanext/gobar_theme/routing.py
@@ -56,7 +56,7 @@ class GobArRouter:
         )
 
     def connect_section(self):
-        self.home_routes.connect('section', '/acerca/seccion/{title}', action='view_about_section')
+        self.home_routes.connect('section', '/acerca/seccion/{title_or_slug}', action='view_about_section')
 
     def connect_google_analytics(self):
         with SubMapper(self.route_map, controller=self.google_analytics_controller) as m:
