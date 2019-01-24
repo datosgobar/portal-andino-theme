d9a5a9f88828faba38c7bbde92358c0710904263
Christian Digiorno <cdigiorno@devartis.com>
2019-01-24 15:27:11 -0300

    Agrego resguardo contra situaciones donde el archivo no esté en el datastore (devuelve texto html)

--- ckanext/gobar_theme/lib/cli.py
+++ ckanext/gobar_theme/lib/cli.py
@@ -134,7 +134,10 @@ class ReuploadResourcesFiles(cli.CkanCommand):
                         total_resources_to_patch += 1
                         resource_file_path = '/tmp/{}'.format(filename)
                         try:
-                            file_content = requests.get('{0}/datastore/dump/{1}'.format(site_url, resource_id)).content
+                            response = requests.get('{0}/datastore/dump/{1}'.format(site_url, resource_id))
+                            if 'text/csv' in response.headers.get('Content-Type'):
+                                raise TypeError("El archivo no se encuentra en el Datastore")
+                            file_content = response.content
                             if not file_content:
                                 raise ValueError('Archivo proveniente de Datastore sin contenido')
                             with open(resource_file_path, 'wb') as resource_file:
