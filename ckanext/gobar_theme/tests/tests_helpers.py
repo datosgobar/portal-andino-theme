3fab251c912e3ca7eafd2b2df2648d6018457b4a
Christian Digiorno <cdigiorno@devartis.com>
2019-02-26 16:01:23 -0300

    Refactor testeo de licencias

--- ckanext/gobar_theme/tests/tests_helpers.py
+++ ckanext/gobar_theme/tests/tests_helpers.py
@@ -1,5 +1,9 @@
+# encoding: utf-8
+
+import os
+import json
+import urllib2
 from mock import patch
-from mockredis import mock_strict_redis_client
 import ckan.tests.factories as factories
 import nose.tools as nt
 from ckanext.gobar_theme.tests import TestAndino
@@ -10,11 +14,7 @@ from ckan.model import license
 
 class TestHelpers(TestAndino.TestAndino):
 
-    def __init__(self):
-        super(TestHelpers, self).__init__()
-
     @patch('ckanext.gobar_theme.helpers.GobArConfigController', GobArConfigControllerForTest)
-    @patch('redis.StrictRedis', mock_strict_redis_client)
     def setup(self):
         super(TestHelpers, self).setup()
         self.admin = factories.Sysadmin()
@@ -22,31 +22,28 @@ class TestHelpers(TestAndino.TestAndino):
 
 class TestLicenseHelpers(TestHelpers):
 
+    def load_licenses(self, url):
+        # No usamos la URL que nos llega por parámetro porque queremos hardcodear el path del JSON
+        script_dir = os.path.dirname(__file__)
+        relative_path = 'tests_config/licenses.json'
+        license_url = "file://{}".format(os.path.join(script_dir, relative_path))
+        response = urllib2.urlopen(license_url)
+        response_body = response.read()
+        license_data = json.loads(response_body)
+        license.LicenseRegister._create_license_list(license_data, '')
+
+    @patch('ckan.model.license.LicenseRegister.load_licenses', load_licenses)
     def __init__(self):
         super(TestLicenseHelpers, self).__init__()
-        licenses_json = [
-            {
-                "id": "PDDL-1.0",
-                "legacy_ids": ["ODC-PDDL-1.0", "odc-pddl"],
-                "title": "Open Data Commons Public Domain Dedication and Licence 1.0 (PDDL)",
-                "url": "https://opendefinition.org/licenses/odc-pddl"
-            },
-            {
-                "id": "ODbL-1.0",
-                "legacy_ids": ["odc-odbl"],
-                "title": "Open Data Commons Open Database License 1.0 (ODbL)",
-                "url": "https://opendefinition.org/licenses/odc-odbl"
-            }
-        ]
-        self.licenses = [license.License(entity) for entity in licenses_json]
+        self.licenses = gobar_helpers.license_options()
 
-    @patch('redis.StrictRedis', mock_strict_redis_client)
     @patch('ckanext.gobar_theme.helpers.GobArConfigController', GobArConfigControllerForTest)
-    def test_license_options_returns_2_licenses(self):
-        nt.assert_equals(len(self.licenses), 2)
+    @patch('ckan.model.license.LicenseRegister.load_licenses', load_licenses)
+    def test_license_options_returns_1_licenses(self):
+        nt.assert_equals(len(self.licenses), 14)
 
-    @patch('redis.StrictRedis', mock_strict_redis_client)
     @patch('ckanext.gobar_theme.helpers.GobArConfigController', GobArConfigControllerForTest)
+    @patch('ckan.model.license.LicenseRegister.load_licenses', load_licenses)
     def test_legacy_license_ids_are_detected(self):
         pddl_license = None
         for license in self.licenses:
@@ -54,14 +51,14 @@ class TestLicenseHelpers(TestHelpers):
                 pddl_license = license
         nt.assert_true(gobar_helpers.id_belongs_to_license('odc-pddl', pddl_license))
 
-    @patch('redis.StrictRedis', mock_strict_redis_client)
     @patch('ckanext.gobar_theme.helpers.GobArConfigController', GobArConfigControllerForTest)
+    @patch('ckan.model.license.LicenseRegister.load_licenses', load_licenses)
     def test_legacy_id_search_returns_correct_license(self):
         license = gobar_helpers.get_license('odc-pddl')
         nt.assert_equals(license.title, u'Open Data Commons Public Domain Dedication and Licence 1.0 (PDDL)')
 
-    @patch('redis.StrictRedis', mock_strict_redis_client)
     @patch('ckanext.gobar_theme.helpers.GobArConfigController', GobArConfigControllerForTest)
+    @patch('ckan.model.license.LicenseRegister.load_licenses', load_licenses)
     def test_license_title_search_returns_correct_title(self):
         nt.assert_equals(gobar_helpers.get_license_title('odc-pddl'),
                          u'Open Data Commons Public Domain Dedication and Licence 1.0 (PDDL)')
