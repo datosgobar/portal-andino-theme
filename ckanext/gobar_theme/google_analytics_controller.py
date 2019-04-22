7cc72bf52cc37c2fec049f72cd2eb51a731e3086
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-04-22 15:20:29 -0300

    Corrijo pylint en google analytics controller

--- ckanext/gobar_theme/google_analytics_controller.py
+++ ckanext/gobar_theme/google_analytics_controller.py
@@ -1,8 +1,8 @@
-import ckanext.googleanalytics.plugin as google_analytics
 from ckan.lib.base import BaseController, c
+import ckanext.googleanalytics.plugin as google_analytics
 
 
 class GobArGAController(BaseController):
-    
+
     def resource_view_embed(self, resource_id):
         google_analytics._post_analytics(c.user, 'CKAN Resource Embed', 'Resource ', resource_id)
