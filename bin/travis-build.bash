06da8b5270ceddbe352d579806d84532876ac2c4
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-05-02 12:30:49 -0300

    Cambio pull por clone

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 THEME_PATH=$(pwd)
-git pull "https://github.com/datosgobar/portal-andino"
+git clone "https://github.com/datosgobar/portal-andino"
 
 cd portal_andino/
 sudo ./dev.sh complete_up --theme_volume_src $THEME_PATH
