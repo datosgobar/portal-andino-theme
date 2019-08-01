7ca08055fd610849705ccf1af65a512b7c414869
Lucas Romero <lromero@devartis.com>
2019-08-01 11:47:15 -0300

    patch de traduccion de meses para tests

---
+++ ckanext/gobar_theme/tests/tools/constants.py
@@ -0,0 +1,14 @@
+MONTH_FUNCTIONS = [
+    (lambda: 'enero'),
+    (lambda: 'febrero'),
+    (lambda: 'marzo'),
+    (lambda: 'abril'),
+    (lambda: 'mayo'),
+    (lambda: 'junio'),
+    (lambda: 'julio'),
+    (lambda: 'agosto'),
+    (lambda: 'septiembre'),
+    (lambda: 'octubre'),
+    (lambda: 'noviembre'),
+    (lambda: 'diciembre'),
+]
