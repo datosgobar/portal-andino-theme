1ac81a8b4e25d82c9225872d04096ed933c79427
Alexis Wolfsdorf <awolfsdorf@devartis.com>
2019-09-06 12:19:46 -0300

    max_length por el form y validación de tamaño por jquery

--- ckanext/gobar_theme/js/organization/form_actions.js
+++ ckanext/gobar_theme/js/organization/form_actions.js
@@ -21,20 +21,25 @@ $(function () {
 
     function formIsValid() {
         $('.missing-field').remove();
-        var titleValid = true;
+        var titleIsEmpty;
+        var titleIsValid;
         var urlValid = true;
-        var errorTemplate = '<div class="missing-field">Completá este dato</div>';
+        var isEmptyErrorTemplate = '<div class="missing-field">Completá este dato</div>';
+        var isTooLongErrorTemplate = '<div class="missing-field">El título es muy largo</div>';
 
         var title = $('#field-name');
-        titleValid = title.val().length > 0;
+        titleIsEmpty = 0 >= title.val().length;
+        titleIsValid = title.val().length <= 100;
 
-        if (!titleValid){
-            title.after(errorTemplate);
+        if (titleIsEmpty){
+            title.after(isEmptyErrorTemplate);
+        } else if (!titleIsValid) {
+            title.after(isTooLongErrorTemplate);
         } else {
-            urlValid = urlValidator(errorTemplate);
+            urlValid = urlValidator(isEmptyErrorTemplate);
         }
 
-        var isValid = titleValid && urlValid;
+        var isValid = titleIsValid && !titleIsEmpty && urlValid;
         if (!isValid) {
             window.scrollTo(0, 0);
         }
