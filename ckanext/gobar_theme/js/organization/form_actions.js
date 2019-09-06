960f21c8a5a132bc517425d7dd4e9841267a11e1
Alexis Wolfsdorf <awolfsdorf@devartis.com>
2019-09-06 15:12:53 -0300

    cambio nombres de variables para ser mas entendible y correccion typo

--- ckanext/gobar_theme/js/organization/form_actions.js
+++ ckanext/gobar_theme/js/organization/form_actions.js
@@ -22,24 +22,24 @@ $(function () {
     function formIsValid() {
         $('.missing-field').remove();
         var titleIsTooShort;
-        var titleIsValid;
+        var titleIsTooLong;
         var urlValid;
-        var isTooShortErrorTemplate = '<div class="missing-field">El título debe tener almenos 2 caracteres</div>';
+        var isTooShortErrorTemplate = '<div class="missing-field">El título debe tener al menos 2 caracteres</div>';
         var isTooLongErrorTemplate = '<div class="missing-field">El título es muy largo</div>';
 
         var title = $('#field-name');
         titleIsTooShort = title.val().length < 2;
-        titleIsValid = title.val().length <= 100;
+        titleIsTooLong = title.val().length > 100;
 
         if (titleIsTooShort){
             title.after(isTooShortErrorTemplate);
-        } else if (!titleIsValid) {
+        } else if (titleIsTooLong) {
             title.after(isTooLongErrorTemplate);
         } else {
             urlValid = urlValidator(isTooShortErrorTemplate);
         }
 
-        var isValid = titleIsValid && !titleIsTooShort && urlValid;
+        var isValid = !titleIsTooLong && !titleIsTooShort && urlValid;
         if (!isValid) {
             window.scrollTo(0, 0);
         }
