3ed9a53814108ebf37e774d53a90872cd2aba999
Christian Digiorno <cdigiorno@devartis.com>
2019-11-14 12:23:30 -0300

    Fix guardado de cron

--- ckanext/gobar_theme/helpers/cron.py
+++ ckanext/gobar_theme/helpers/cron.py
@@ -1,5 +1,4 @@
 #!coding=utf-8
-import getpass
 
 from crontab import CronTab
 
@@ -7,7 +6,7 @@ from crontab import CronTab
 def search_for_cron_jobs_and_remove(comment_to_search_for):
     # Buscamos y eliminamos los cron jobs que contengan el comment especificado por parámetro
     if comment_to_search_for:
-        cron = CronTab(get_current_terminal_username())
+        cron = CronTab('www-data')
         jobs_with_specified_comment = cron.find_comment(comment_to_search_for)
         cron.remove(jobs_with_specified_comment)
         cron.write()
@@ -16,12 +15,8 @@ def search_for_cron_jobs_and_remove(comment_to_search_for):
 def create_or_update_cron_job(command, hour, minute, comment=''):
     if comment:
         search_for_cron_jobs_and_remove(comment)
-    cron = CronTab(get_current_terminal_username())
+    cron = CronTab('www-data')
     job = cron.new(command=command, comment=comment)
     job.hour.on(hour)
     job.minute.on(minute)
     cron.write()
-
-
-def get_current_terminal_username():
-    return getpass.getuser()
